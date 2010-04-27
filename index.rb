73f48621635ac09e696075f079633c04a3aaea2a
Irina Dumitrascu <me@dira.ro>
2010-04-27 18:24:18 -0300

    primitive views

--- index.rb
+++ index.rb
@@ -2,33 +2,50 @@ require 'rubygems'
 require 'sinatra'
 require 'json'
 require 'net/http'
+require 'haml'
+require 'sass'
 
 get '/' do
   "Must add help here"
 end
 
-get '/:username' do
-  "Albums of #{params[:username]}: #{albums(params[:username])}"
+get '/stylesheet.css' do
+  content_type 'text/css', :charset => 'utf-8'
+  sass :stylesheet
 end
 
-get '/:username/:album' do
-  "Photos of #{params[:username]}, album #{params[:album]}: #{photos(params[:username], params[:album])}"
+get '/:username/?' do
+  @albums = albums(params[:username])
+  haml :user
+end
+
+get '/:username/:album/?' do
+  @photos = photos(params[:username], params[:album])
+  haml :photos
+end
+
+
+def albums_url(username)
+  URI.parse("http://picasaweb.google.com/data/feed/api/user/#{URI.escape(username)}?alt=json")
+end
+
+def photos_url(username, album)
+  URI.parse("http://picasaweb.google.com/data/feed/api/user/#{URI.escape(username)}/albumid/#{URI.escape(album)}?alt=json")
 end
 
 def albums(username)
-  uri = URI.parse("http://picasaweb.google.com/data/feed/api/user/#{URI.escape(username)}?alt=json")
-  content = Net::HTTP.get(uri)
-  albums = JSON.parse(content)['feed']['entry']
-  albums.map do |album|
-    [album["title"]["$t"], album["gphoto$id"]["$t"], album["media$group"]["media$thumbnail"][0]["url"]]
+  content = Net::HTTP.get(albums_url(username))
+  JSON.parse(content)['feed']['entry'].map do |album|
+    { :title => album["title"]["$t"],
+      :id => album["gphoto$id"]["$t"],
+      :thumbnail => album["media$group"]["media$thumbnail"][0]["url"]
+    }
   end
 end
 
 def photos(username, album)
-  uri = URI.parse("http://picasaweb.google.com/data/feed/api/user/#{URI.escape(username)}/albumid/#{URI.escape(album)}?alt=json")
-  content = Net::HTTP.get(uri)
-  photos = JSON.parse(content)['feed']['entry']
-  photos.map do |photo|
-    photo["content"]["src"]
+  content = Net::HTTP.get(photos_url(username, album))
+  JSON.parse(content)['feed']['entry'].map do |photo|
+    { :src => photo["content"]["src"].gsub(/(\/[^\/]+)$/, '/s400\1') }
   end
 end
